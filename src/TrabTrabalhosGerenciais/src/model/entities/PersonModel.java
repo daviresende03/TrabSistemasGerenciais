5af181cd8c6ea713f370bce3de289818eabf7e97
Davi Resende <107009159+daviresende03@users.noreply.github.com>
2024-05-09 19:50:48 -0300

    feat: Criação de estrutura inicial.

---
+++ src/TrabTrabalhosGerenciais/src/model/entities/PersonModel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.entities;
+
+import model.valueObjects.Address;
+
+/**
+ *
+ * @author Davi
+ */
+public abstract class PersonModel {
+    private String nome;
+    private Address address;
+    
+}

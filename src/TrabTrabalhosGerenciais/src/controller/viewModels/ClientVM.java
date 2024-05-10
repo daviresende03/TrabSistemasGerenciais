aa3b1b9c49ca7e3ff3edf7443bd923f6f3874ecf
Davi Resende <107009159+daviresende03@users.noreply.github.com>
2024-05-09 21:17:25 -0300

    feat: Criação viewModels.

---
+++ src/TrabTrabalhosGerenciais/src/controller/viewModels/ClientVM.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.viewModels;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Davi
+ */
+public class ClientVM {
+    public int id;
+    public String name;
+    public String typeDocument;
+    public String numberDocument;
+    public String type;
+    public String street;
+    public String number;
+    public String neighborhood;
+    public String city;
+    public String state;
+    public String postalCode;
+    public String country;
+    public Date birthDate;
+}
---
+++ src/TrabTrabalhosGerenciais/src/controller/viewModels/ProductVM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.viewModels;
+
+
+/**
+ *
+ * @author Davi
+ */
+public class ProductVM {
+    public int id;
+    public String name;
+    public String type;
+    public UnitVM unit;
+    public double costPrice;
+    public double salePrice;
+    public double stock;
+}
